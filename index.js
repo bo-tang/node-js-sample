622a7d65f752f59e165eb7b99c34e11ed41397e7
Bo Tang <botang@hiretual.com>
2017-12-15 17:58:09 -0800

    nodejs experiments and initial files created

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'ejs')
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', function(req, res) {
+  // ejs engine picks ./views/index.ejs
+  res.render('index');
+})
+
+app.post('/', function (req, res) {
+  console.log(req.body.city);
+  res.render('index', {city:req.body.city});  
 })
 
 app.listen(app.get('port'), function() {
